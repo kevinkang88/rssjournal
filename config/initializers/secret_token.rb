e5bb267ba58be6bfc77f6c17822c482d05b57c40
Kevin Kang <kevinkang88@gmail.com>
2014-08-18 19:57:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rssjournal::Application.config.secret_token = '13c60f260ab7abe4c525a2183785d88ab51642c753931be4c01bea5911bc9b4773864323d92ddad90059511ce05bab46ec7825790d9192d9a3eab6cd9c942ea5'
